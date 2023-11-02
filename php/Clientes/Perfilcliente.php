4eccdc053bae89b410911bf362b6614c52f621db
Valdess <132627914+FernandoValdes01@users.noreply.github.com>
2023-11-02 17:34:08 -0400

    semana2 sprint3
    todas las tareas mias realizadas en esta semana en el ambito de cliente php , las tareas consisten en lass siguientes 
    1)Perfilcliente.php: agrege la consulta para que elimine su cuenta y todos los datos em la bd donde se encuentren enlazados a la cuenta
    2)menu.php:agrege que se pueda ver la informacion de los trabajadores un poco mas extensa 
    3)menus particulares (cuenta lo mismo que el 2 pero como son mas archivos los poco por separado)
    4)solicitudservicio:el cliente ahora con la nueva base de datos ya puede hacer sus solicitudes de servicio :) 
    5)calificarpedido:tal como e, ,nombre lo dice modifique el archivo (lo hice denuevo) de nuestro compañero jaziel roman y agrege varias cosas que le faltaban al archivo

--- php/Clientes/Perfilcliente.php
+++ php/Clientes/Perfilcliente.php
@@ -1,6 +1,10 @@
-
 <?php
 session_start();
+
+if (!(isset($_SESSION['Correo_Cliente']))) {
+    header("Location: inicioclientes.php");
+    exit();
+}
 
 $servername = "localhost";
 $username = "root";
@@ -10,57 +14,78 @@ $dbname = "techome";
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
-    echo "<script>alert('Conexión fallida: ".$conn->connect_error."');</script>";
+    echo "<script>alert('Conexión fallida: " . $conn->connect_error . "');</script>";
 }
 
+$idcliente = $_SESSION['ID_cliente'];
 $nombre = isset($_SESSION['nombre_Cliente']) ? $_SESSION['nombre_Cliente'] : '';
 $correo = isset($_SESSION['Correo_Cliente']) ? $_SESSION['Correo_Cliente'] : '';
+$contraseña = $_SESSION['contraseña'];
 
 if (isset($_POST['actualizar'])) {
-    $nombre = $_POST['nombre'];
-    $sql = "UPDATE clientes SET nombre_Cliente='$nombre' WHERE Correo_Cliente='$correo'";
-    if ($conn->query($sql) === TRUE) {
-        echo "<script>alert('Registro actualizado exitosamente');</script>";
+    $nuevo_nombre = $_POST['nombre'];
+    $nuevo_correo = $_POST['correo'];
+
+    $sql_actualizar = "UPDATE clientes SET nombre_Cliente='$nuevo_nombre', Correo_Cliente='$nuevo_correo' WHERE ID_cliente='$idcliente'";
+
+    if ($conn->query($sql_actualizar) === TRUE) {
+        $_SESSION['nombre_Cliente'] = $nuevo_nombre; 
+        $_SESSION['Correo_Cliente'] = $nuevo_correo;
+        echo "<script>alert('Registro actualizado exitosamente'); window.location.href = window.location.href;</script>";
     } else {
-        echo "<script>alert('Error al actualizar el registro: ".$conn->error."');</script>";
+        echo "<script>alert('Error al actualizar el registro: " . $conn->error . "');</script>";
     }
 }
+
 
 if (isset($_POST['cambiar'])) {
-    $contrasena = $_POST['contrasena'];
-    $sql = "UPDATE clientes SET contraseña='$contrasena' WHERE Correo_Cliente='$correo'";
-    if ($conn->query($sql) === TRUE) {
-        echo "<script>alert('Contraseña actualizada exitosamente');</script>";
+    $nueva_contraseña = $_POST['contrasena'];
+    $sql_cambiar = "UPDATE clientes SET contraseña='$nueva_contraseña' WHERE ID_cliente='$idcliente'";
+
+    if ($conn->query($sql_cambiar) === TRUE) {
+        $_SESSION['contraseña'] = $nueva_contraseña; 
+        echo "<script>alert('Contraseña actualizada exitosamente'); window.location.href = window.location.href;</script>";
     } else {
-        echo "<script>alert('Error al actualizar la contraseña: ".$conn->error."');</script>";
+        echo "<script>alert('Error al actualizar la contraseña: " . $conn->error . "');</script>";
     }
 }
-
 if (isset($_POST['eliminar_cuenta'])) {
     $contrasena = $_POST['password'];
-    $sql = "SELECT contraseña FROM clientes WHERE Correo_Cliente='$correo'";
-    $result = $conn->query($sql);
-    if ($result->num_rows > 0) {
-        $row = $result->fetch_assoc();
-        if ($contrasena == $row['contraseña']) {
-            $sql_delete = "DELETE FROM clientes WHERE Correo_Cliente='$correo'";
-            if ($conn->query($sql_delete) === TRUE) {
-                session_destroy(); 
-                echo "<script>alert('Cuenta eliminada exitosamente');</script>";
+
+    $sql_password = "SELECT contraseña, ID_cliente FROM clientes WHERE Correo_Cliente='$correo'";
+    $result_password = $conn->query($sql_password);
+
+    if ($result_password->num_rows > 0) {
+        $row_password = $result_password->fetch_assoc();
+        $password_from_db = $row_password["contraseña"];
+        $id_cliente = $row_password["ID_cliente"];
+
+        if ($contrasena == $password_from_db) {
+            $sql_delete_pedido = "DELETE FROM pedido_aceptado WHERE ID_solicitud IN (SELECT ID_solicitud FROM solicitudservicio WHERE ID_cliente='$id_cliente')";
+            $conn->query($sql_delete_pedido);
+
+
+            $sql_delete_solicitud = "DELETE FROM solicitudservicio WHERE ID_cliente='$id_cliente'";
+            $conn->query($sql_delete_solicitud);
+
+            $sql_delete_cliente = "DELETE FROM clientes WHERE Correo_Cliente='$correo'";
+            if ($conn->query($sql_delete_cliente) === TRUE) {
+                echo "<script>alert('Cuenta eliminada exitosamente'); window.location.href = 'Menu.php';</script>";
+                exit();
             } else {
-                echo "<script>alert('Error al eliminar la cuenta: ".$conn->error."');</script>";
+                echo "<script>alert('Error al eliminar la cuenta: " . $conn->error . "');</script>";
             }
         } else {
-            echo "<script>alert('La contraseña ingresada no coincide con la contraseña actual');</script>";
+            echo "<script>alert('La contraseña ingresada no coincide con la contraseña de la cuenta');</script>";
         }
     } else {
-        echo "<script>alert('No se encontró ninguna cuenta asociada a este correo electrónico');</script>";
+        echo "<script>alert('Error al obtener la contraseña de la base de datos');</script>";
     }
 }
 
 if (isset($_POST['logout'])) {
-    session_destroy(); 
-    echo "<script>alert('Has cerrado la sesión exitosamente'); window.location.href = 'login.php';</script>";
+    session_destroy();
+    echo "<script>alert('Has cerrado la sesión exitosamente'); window.location.href = 'Menu.php';</script>";
     exit();
 }
 
@@ -291,49 +316,108 @@ $conn->close();
 <body>
     <header>
         <h1>Configuración del Cliente - TecHome</h1>
-    </header>
-    <div class="container">
-        <div class="profile-section">
-            <h2>Tu Perfil</h2>
-            <div class="container">
-                <div class="trabajo">
-                    <h3></h3>
-                    <label for="nombre">Nombre:</label>
-                    <span id="nombre-actual"><?php echo $nombre; ?></span>
-                    <br>
-                    <label for="email">Correo Electrónico (Gmail):</label>
-                    <span id="email-actual"><?php echo $correo; ?></span>
-                </div>
+        </header>
+
+<div id="menu">
+    <ul>
+        <li></li>
+        <li id="config"> <a href="Menu.php">Inicio</a></li>
+        <li id="config"> <a href="Perfilcliente.php">Perfil</a></li>
+        <li id="quienes-somos"> <a href="NuestrahistoriaC1.html">Quienes somos</a></li>
+        <li id="direcciones"> <a href="DireccionesActualizado.html">Direcciones</a></li>
+        <li id="soporte"> <a href="soportec1.html">Soporte</a></li>
+        <li id="politica_de_privacidadc"> <a href="politica de privacidadc1.html">Politíca de Privacidad</a></li>
+    </ul>
+</div>
+
+
+<div class="container">
+    <div class="profile-section">
+        <h2>Tu Perfil</h2>
+        <div class="container">
+            <div class="trabajo">
+                <h3></h3>
+                <label for="nombre">Nombre:</label>
+                <span id="nombre-actual"><?php echo $nombre; ?></span>
+                <br>
+                <label for="email">Correo Electrónico (Gmail):</label>
+                <span id="email-actual"><?php echo $correo; ?></span>
             </div>
-            <form method="post" action="">
-                <div id="changeinf">
-                    <h2>Cambiar Nombre</h2>
-                    <label for="nombre">Nuevo Nombre:</label>
-                    <input type="text" id="nombre" name="nombre" value="<?php echo $nombre; ?>" required>
-                    <button type="submit" name="actualizar">Actualizar Perfil</button>
-                </div>
-            </form>
-            <form method="post" action="">
-                <div id="passtext">
-                    <h2>Cambiar Contraseña</h2>
-                    <label for="contrasena">Nueva Contraseña:</label>
-                    <input type="password" id="contrasena" name="contrasena" required>
-                    <button type="submit" name="cambiar">Cambiar Contraseña</button>
-                </div>
-            </form>
-            <h2>Cerrar Sesión</h2>
-            <form method="post" action="">
-                <button class="button-style" type="submit" name="logout">Cerrar Sesión</button>
-            </form>
-            <form method="post" action="">
-                <div id="eliminar_cuenta">
-                    <h2>Eliminar Cuenta</h2>
-                </div>
-                <label for="password">Contraseña:</label>
-                <input type="password" id="password" name="password" required>
-                <button type="submit" name="eliminar_cuenta">Eliminar Cuenta</button>
-            </form>
         </div>
+        <form method="post" action="">
+            <div id="changeinf">
+                <h2>Cambiar Nombre y Correo Electrónico</h2>
+                <label for="nombre">Nuevo Nombre:</label>
+                <input type="text" id="nombre" name="nombre" value="<?php echo $nombre; ?>" required>
+                <br>
+                <label for="correo">Nuevo Correo Electrónico:</label>
+                <input type="email" id="correo" name="correo" value="<?php echo $correo; ?>" required>
+                <button type="submit" name="actualizar">Actualizar Perfil</button>
+            </div>
+        </form>
+        <form method="post" action="">
+            <div id="passtext">
+                <h2>Cambiar Contraseña</h2>
+                <label for="contrasena">Nueva Contraseña:</label>
+                <input type="password" id="contrasena" name="contrasena" required>
+                <button type="submit" name="cambiar">Cambiar Contraseña</button>
+            </div>
+        </form>
+        <h2>Cerrar Sesión</h2>
+        <form method="post" action="">
+            <button class="button-style" type="submit" name="logout">Cerrar Sesión</button>
+        </form>
+        <form method="post" action="">
+            <div id="eliminar_cuenta">
+                <h2>Eliminar Cuenta</h2>
+            </div>
+            <label for="password">Contraseña:</label>
+            <input type="password" id="password" name="password" required>
+            <button type="submit" name="eliminar_cuenta">Eliminar Cuenta</button>
+        </form>
     </div>
+</div>
+
+<div id="menu-toggle">&#9776;</div> 
+        <script>
+        const menu = document.getElementById('menu');
+        const menuToggle = document.getElementById('menu-toggle');
+        const configuracion_de_perfil = document.getElementById('config');
+        const quienesSomos = document.getElementById('Nuestrahistoria2');
+        const direcciones = document.getElementById('direcciones');
+        const soporte = document.getElementById('soporte');
+        const politica_de_privacidad = document.getElementById('politica_de_privacidadc');
+        
+        menuToggle.addEventListener('click', () => {
+            menu.classList.toggle('active'); 
+        });
+        
+        configuracion_de_perfil.addEventListener('click', () => {
+            console.log('Clic en Configuración');
+        });
+        
+        quienesSomos.addEventListener('click', () => {
+            console.log('Clic en Quienes Somos');
+        });
+
+        politica_de_privacidadc.addEventListener('click', () => {
+            console.log('Clic en Politicas de Privacidad')
+        });
+
+        direcciones.addEventListener('click', ()=> {
+            console.log('Clic en Direcciones')
+        });
+
+        function fetchData(order) {
+            fetch('gettrabajadores.php?order=' + order)
+                .then(response => response.text())
+                .then(data => {
+                    document.getElementById('trabajadoresContainer').innerHTML = data;
+                })
+                .catch(error => {
+                    console.error('Hubo un error al obtener los datos: ' + error);
+                });
+        }
+        </script>
 </body>
 </html>

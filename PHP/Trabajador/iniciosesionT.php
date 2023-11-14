11d89681104827d859e827c878cfc75d81995ba6
franciscobenavides876 <134728916+franciscobenavides876@users.noreply.github.com>
2023-11-14 01:20:20 -0300

    Actualizacion y Imagenes

--- PHP/Trabajador/iniciosesionT.php
+++ PHP/Trabajador/iniciosesionT.php
@@ -5,42 +5,47 @@ $contraseña = "";
 $basededatos = "techome";
 
 $conexion = new mysqli($server, $usuario, $contraseña, $basededatos);
+$mensaje = ""; 
+
 
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
 }
 
-$mensaje = "";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $correo = $_POST['Correo_Trabajador'];
-    $contrasena = $_POST['contrasena'];
 
-    $sql = "SELECT Correo_Trabajador, contraseña, Nombre_Trabajador FROM trabajador WHERE Correo_Trabajador = '$correo'";
+    $correo = $_POST['correo'];
+    $contrasena = $_POST['contrasena'];
+    $sql = "SELECT ID_cliente,Correo_Cliente, contraseña,nombre_Cliente FROM clientes WHERE Correo_Cliente = '$correo'";
     $result = $conexion->query($sql);
 
     if ($result === false) {
         die("Error en la consulta SQL: " . $conexion->error);
     }
 
-    $mensaje = "Error en el inicio de sesión. La contraseña es incorrecta.";
-
     if ($result->num_rows > 0) {
         $row = $result->fetch_assoc();
         $contrasena_db = $row['contraseña'];
-        $nombre = $row["Nombre_Trabajador"];
-
+        $nombre=$row["nombre_Cliente"];
+        $ID_cliente=$row["ID_cliente"];
         if ($contrasena == $contrasena_db) {
             $mensaje = "Inicio de sesión exitoso. ¡Bienvenido!";
             session_start();
-            $_SESSION['Correo_Trabajador'] = $correo;
+            $_SESSION['Correo_Cliente'] = $correo;
             $_SESSION['contraseña'] = $contrasena_db;
-            $_SESSION['Nombre_Trabajador'] = $nombre;
-            header('Location: MenuTrabajador.php');
+            $_SESSION['nombre_Cliente'] = $nombre;
+            $_SESSION['ID_cliente'] = $ID_cliente;
+            header('Location: Menu.php'); 
             exit();
+        } else {
+            $mensaje = "Error en el inicio de sesión. Comprueba tus credenciales.";
         }
+    } else {
+        $mensaje = "Error en el inicio de sesión. El correo no existe en la base de datos.";
     }
 }
+
 
 $conexion->close();
 ?>
@@ -53,79 +58,168 @@ $conexion->close();
     <title>TecHome</title>
     <style>
         body {
-            font-family: Arial, sans-serif;
-            background-color: #f0f0f0;
             margin: 0;
             padding: 0;
-            display: flex;
-            justify-content: center;
-            align-items: center;
+            font-family: 'Verdana', 'Monaco', sans-serif;
+            background-image: url('Imagenes/background3.png');
             height: 100vh;
+            overflow: hidden;
         }
 
-        .Box {
-            background-color: #fff;
-            padding: 20px;
-            border-radius: 4px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        .Formulario{
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            width: 400px;
+            background: white;
+            border-radius: 10px;
+        }
+
+        .Formulario h1{
             text-align: center;
+            padding: 0 0 20px 0;
+            border-bottom: 1px solid silver;
         }
 
-        .Form {
-            max-width: 300px;
-            margin: 0 auto;
+        .Formulario form{
+            padding: 0 40px;
+            box-sizing: border-box;
         }
 
-        label,
-        input {
-            display: block;
-            margin-bottom: 10px;
+        form .InputBox{
+            position: relative;
+            border-bottom: 2px solid silver;
+            margin: 30px 0;
         }
 
-        button {
-            background-color: #007bff;
-            color: #fff;
+        .InputBox input{
+            width: 100%;
+            padding: 0 5px;
+            height: 40px;
+            font-size: 16px;
             border: none;
-            border-radius: 4px;
-            padding: 10px 20px;
+            background: none;
+            outline: none;
+        }
+
+        .InputBox label{
+            position: absolute;
+            top: -15%;
+            left: 5px;
+            color: #adadad;
+            transform: translate(-10%);
+            font-size: 16px;
+            pointer-events: none;
+            transition: .5s;
+        }
+
+        .InputBox span::before{
+            content: '';
+            position: absolute;
+            top: 40px;
+            left: 0;
+            width: 100%;
+            height: 2px;
+            background: #1A5276;
+            transition: .5s;
+        }
+
+        .InputBox input:focus ~ label,
+        .InputBox input:focus ~ label{
+            top: -20px;
+            color: #1A5276;
+        }
+
+        .InputBox input:focus ~ span:before,
+        .InputBox input:focus ~ span:before{
+            width: 100%;
+        }
+
+        .link{
+            margin: -5px 0 20px 5px;
+            color: #2874A6; 
             cursor: pointer;
         }
+
+        .link:hover{
+            text-decoration: underline;
+        }
+
+        button[type="submit"]{
+            width: 100%;
+            height: 50px;
+            border: 1px solid;
+            background: #0061FF ;
+            border-radius: 25px;
+            font-size: 10px;
+            margin: 2px;
+            color: white;
+            cursor: pointer;
+            outline: none;
+        }
+
+        button[type="submit"]:hover{
+            border-color: #0061FF ;
+            transition: .5s;
+        }
+
+        button[type="button"]{
+            width: 100%;
+            height: 50px;
+            border: 1px solid;
+            background: #00BFFF;
+            border-radius: 25px;
+            font-size: 10px;
+            margin: 2px;
+            color: white;
+            cursor: pointer;
+            outline: none;
+        }
+
+        button[type="button"]:hover{
+            border-color: #00BFFF;
+            transition: .5s;
+        }
+
     </style>
 </head>
 <body>
-    <div class="Box">
-        <div class="Form">
-            <h2>Inicia Sesión</h2>
-            <form method="POST" action="iniciosesionT.php">
-                <div class="InputBox">
-                    <label for="Correo_Trabajador">Correo electrónico:</label>
-                    <input type="email" id="Correo_Trabajador" name="Correo_Trabajador" required>
-                </div>
-                <div class="InputBox">
-                    <label for="contrasena">Contraseña:</label>
-                    <input type="password" id="contrasena" name="contrasena" required>
-                </div>
-                <div class="link">
-                    <a href="#">¿Olvidaste la clave?</a>
-                </div>
-                <div class="Buttons">
-                    <button type="submit">Listo</button>
-                </div>
-            </form>
-            <button type="button" onclick="window.location.href='registrotrabajador.php'">Registrarse</button>
-            <?php echo $mensaje; ?>
-        </div>
+    <div class="Formulario">
+        <h1>Inicia Sesión</h1>
+        <form method="POST" action="inicioclientes.php"> <!-- Reemplaza 'nombre_de_esta_pagina.php' con el nombre de tu archivo PHP -->
+            <div class="InputBox">
+                <input type="email" id="correo" name="correo" required> <!-- Agregamos el atributo 'name' para que los datos se envíen correctamente -->
+                <label for="correo">Correo electrónico:</label>
+            </div>
+            <div class="InputBox">
+                <input type="password" id="contrasena" name="contrasena" required> <!-- Agregamos el atributo 'name' para que los datos se envíen correctamente -->
+                <label for="contrasena">Contraseña:</label> <!-- Corregimos el id de la etiqueta 'input' -->
+            </div>
+            <div class="link">
+                <a href="#">¿Olvidaste la clave?</a>
+            </div>
+            <div class="Buttons">
+                <button type="submit">Listo</button>
+                <button type="button" onclick="window.location.href=''">Registrarse</button>
+                <?php echo $mensaje;  ?>
+            </div>
+        </form>
     </div>
 
-    <script>
-        function redirigirAMenuTrabajador() {
-            window.location.href = 'MenuTrabajador.php';
+        <script>
+        function moveLabelUp(inputId) {
+            const input = document.getElementById(inputId);
+            const label = input.nextElementSibling; // Get the label element next to the input
+            if (input.value.trim() !== '') {
+                label.style.transform = 'translate(-10%)'; // Move label up
+                label.style.fontSize = '16px';
+            } else {
+                label.style.transform = ''; // Reset label position
+                label.style.fontSize = '16px';
+            }
         }
-
-        document.addEventListener('DOMContentLoaded', function() {
-            const botonListo = document.querySelector('.Buttons button');
-            botonListo.addEventListener('click', redirigirAMenuTrabajador);
-        });
     </script>
+    </div>
 </body>
 </html>

d7945ad40197abfce24ea1e47a4073482d39e969
franciscobenavides876 <134728916+franciscobenavides876@users.noreply.github.com>
2023-11-15 01:43:10 -0300

    Actualizacion iniciadministradores.php

--- Webphp/Administrador/iniciadministradores.php
+++ Webphp/Administrador/iniciadministradores.php
@@ -34,7 +34,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $_SESSION['nombre'] = $nombre;
             $_SESSION['cargo'] = $cargo;
             $_SESSION['contrasena'] = $contrasena_db;
-            header('Location: Postulantes.php');
+            header('Location: menuadmin.php');
             exit();
         } else {
             $mensaje = "Error en el inicio de sesión. Comprueba tus credenciales.";
@@ -54,234 +54,159 @@ $conexion->close();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TecHome</title>
     <style>
-        body {
-            font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
-            background-color: #27496D;
-            color: #F1EFEF;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 99vh;
-        }
-
-        header {
-            background-color: #142850;
-            border: 2px solid #2C74B3;
-            text-align: left;
-            margin-top: 1%;
-            margin-bottom: 1%;
-            margin-left: 1%;
-            margin-right: 6%;
-            padding: 3%;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 99vh;
-            border-radius: 9px;
-        }
-
-        footer{
-            background-color: #142850;
-            border: 2px solid #2C74B3;
-            text-align: center;
-            margin: 1%;
-            padding: 2%;
-            border-radius: 9px;
-            font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
-        }
-
-        a{
-            text-decoration: none;
-            color: #fff;
-        }
-
-        button.button-style {
-        background-color: #142850;
-        border: 2px solid #2C74B3;
-        color: #F1EFEF;
-        border-radius: 5px;
-        padding: 0.5%;
-        width: 99%;
-        margin-bottom: 9px;
-        }
-        input{
-            background-color: #142850;
-            border: 2px solid #2C74B3;
-            border-radius: 9px;
-        }
-
-        section{
-            background-color: #0A2647;
-            border: 3px solid #144272;
-            padding-top: 5%;
-            padding-bottom: 5%;
-            padding-left: 5%;
-            padding-right: 6%;
-            border-radius: 9px;
-        }
-
-        button:hover{
-            background-color: #2C74B3;
-            color: #fff;
-        }
-
-        .secondbutton{
-            margin-bottom: 9%;
-        }
-
-        .container {
-            max-width: 960px;
-            margin-top: auto;
-            margin-bottom: auto;
-            margin-left: auto;
-            margin-right: auto;
-            padding: 20px;
-        }
-        .profile-section {
-            margin-top: 3%;
-        }
-        .profile-section h2 {
-            margin-bottom: 9px;
-        }
-        .profile-section label {
-            display: block;
-            margin-bottom: 9px;
-        }
-        .profile-section input[type="text"],
-        .profile-section input[type="password"],
-        .profile-section input[type="email"] {
-            width: 99%;
-            padding: 9px;
-            margin-bottom: 20px;
-            border-radius: 3px;
-        }
-        .profile-section button {
-            float: right;
-            padding: 9px 20px;
-            border: none;
-            border-radius: 3px;
-            cursor: pointer;
-        }
-        h1 {
+        body{
             margin: 0;
+            padding: 0;
+            font-family: 'Verdana', 'Monaco', sans-serif;
+            background: linear-gradient(150deg, #FF4F00, #E58729);
+            color: #444444;
+            height: 100vh;
+            overflow: hidden;
+        }  
+
+        .Formulario{
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            width: 400px;
+            background: white;
+            border-radius: 10px;
         }
 
-        .trabajador-info {
+        .Formulario h1{
+            color: #444444;
             text-align: center;
-            padding: 20px;
-        }
-        .trabajador-avatar {
-            border-radius: 50%;
-            width: 99px;
-            height: 99px;
-            background-color: #fff;
-            display: inline-block;
-        }
-        .trabajador-nombre {
-            font-size: 24px;
-            margin-top: 9px;
-        }
-        .trabajos-disponibles {
-            background-color: #fff;
-            padding: 20px;
-            margin: 20px;
-            border-radius: 5px;
-            box-shadow: 0 0 9px rgba(0, 0, 0, 0.1);
-        }
-        ul {
-            list-style-type: none;
-            padding: 0;
-        }
-        li {
-            list-style-type: none;
-            padding: 0;
-        }
-        .trabajo {
-            background-color: #0A2647;
-            border: 3px solid #144272;
-            padding: 3%;
-            border-radius: 9px;
         }
 
-        #passtext{
-            margin-top: 20px;
-            padding-top: 50px;
-        }
-
-        #menu {
-            position: fixed;
-            top: 0;
-            right: -303px; 
-            width: 300px;
-            height: 99%;
-            background-color: #142850;
-            border: 2px solid #2C74B3;
-            border-radius: 9px;
-            color: #fff;
-            transition: right 0.3s; 
-        }
-        
-        #menu.active {
-            right: 0; 
-        }
-        
-        #menu ul {
-            list-style: none;
-            padding: 0;
-        }
-        
-        #menu ul li {
-            padding: 15px;
-            text-align: left;
-            margin-left: 5%;
-            cursor: pointer;
-            border-bottom: 1px solid #142850;
-        }
-        
-        #content {
-            padding: 20px;
+        .Formulario p{
+            font-size: 25px;
             text-align: center;
-            margin-top: 9px;
-            margin-bottom: 9px;
+            padding: 0 0 20px 0;
+            border-bottom: 1px solid silver;
         }
-        
-        #menu-toggle {
-            position: fixed;
-            top: 20px;
-            right: 20px;
+
+        .Formulario form{
+            padding: 0 40px;
+            box-sizing: border-box;
+        }
+
+        form .InputBox{
+            position: relative;
+            border-bottom: 2px solid silver;
+            margin: 30px 0;
+        }
+
+        .InputBox input{
+            width: 100%;
+            padding: 0 5px;
+            height: 40px;
+            font-size: 16px;
+            border: none;
+            background: none;
+            outline: none;
+        }
+
+        .InputBox label{
+            position: absolute;
+            top: -15%;
+            left: 5px;
+            color: #adadad;
+            transform: translate(-10%);
+            font-size: 16px;
+            pointer-events: none;
+            transition: .5s;
+        }
+
+        .InputBox span::before{
+            content: '';
+            position: absolute;
+            top: 40px;
+            left: 0;
+            width: 100%;
+            height: 2px;
+            background: #1A5276;
+            transition: .5s;
+        }
+
+        .InputBox input:focus ~ label,
+        .InputBox input:focus ~ label{
+            top: -20px;
+            color: #1A5276;
+        }
+
+        .InputBox input:focus ~ span:before,
+        .InputBox input:focus ~ span:before{
+            width: 100%;
+        }
+
+        .link{
+            margin: -5px 0 20px 5px;
+            color: #2874A6; 
             cursor: pointer;
-            color: #fff;
-            padding: 1%;
-            margin-top: 1%;
-            margin-right: 1%;
-            background-color: #142850;
-            border: 2px solid #2C74B3;
-            border-radius: 9px;
         }
+
+        .link:hover{
+            text-decoration: underline;
+        }
+
+        button[type="submit"]{
+            width: 100%;
+            height: 50px;
+            border: 1px solid;
+            background: #FF4F00;
+            border-radius: 25px;
+            font-size: 10px;
+            margin: 2px;
+            color: white;
+            cursor: pointer;
+            outline: none;
+        }
+
+        button[type="submit"]:hover{
+            border-color: #E58729;
+            transition: .5s;
+        }
+
     </style>
 </head>
 <body>
-    <div class="Box">
-        <div class="Form">
-            <h2>Inicia Sesión</h2>
-            <form method="POST" action="iniciadministradores.php">
-                <div class="InputBox">
-                    <label for="rut">RUT:</label>
-                    <input type="text" name="rut" required><br>
-                </div>
-                <div class="InputBox">
-                    <label for="contrasena">Contraseña:</label>
-                    <input type="password" name="contrasena" required><br>
-                </div>
-                <div class="link">
-                    <a href="#">¿Olvidaste la clave?</a>
-                </div>
-                <div class="Buttons"></div>
+    <div class="Formulario">
+        <h1>Inicia Sesión</h1>
+        <p>Administrador</p>
+        <form method="POST" action="inicioAdministrador.php">
+            <div class="InputBox">
+                <input type="text" id="rut" name="rut" required>
+                <label for="rut">Rut:</label>
+            </div>
+        
+            <div class="InputBox">
+                <input type="password" id="contrasena" name="contrasena" required> <!-- Agregamos el atributo 'name' para que los datos se envíen correctamente -->
+                <label for="contrasena">Contraseña:</label> <!-- Corregimos el id de la etiqueta 'input' -->
+            </div>
+            <div class="link">
+                <a href="#">¿Olvidaste la clave?</a>
+            </div>
+            <div class="Buttons">
                 <button type="submit">Listo</button>
-            </form>
-            <button type="button" onclick="window.location.href='registroadministradores.php'">Registrarse</button>
-            <?php echo $mensaje;  ?>
-        </div>
+                <?php echo $mensaje;  ?>
+            </div>
+        </form>
+    </div>
+
+        <script>
+        function moveLabelUp(inputId) {
+            const input = document.getElementById(inputId);
+            const label = input.nextElementSibling; // Get the label element next to the input
+            if (input.value.trim() !== '') {
+                label.style.transform = 'translate(-10%)'; // Move label up
+                label.style.fontSize = '16px';
+            } else {
+                label.style.transform = ''; // Reset label position
+                label.style.fontSize = '16px';
+            }
+        }
+    </script>
     </div>
 </body>
 </html>
